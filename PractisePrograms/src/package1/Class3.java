87c1c398c74a20d47e4dbc7c857c483232b808bf
Subbu <Subbu@DESKTOP-FJI47JQ.Home>
2023-01-08 00:53:10 +0000

    commit_2

---
+++ PractisePrograms/src/package1/Class3.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("LinkedIn_practise programmes ");
+
+	}
+
+}
